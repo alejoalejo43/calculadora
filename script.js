5c4c183c4cb91aaaab217db41c481b33217f03ab
Alejandro Castillo <alejandrocastillo.v@gmail.com>
2019-10-18 05:34:54 -0500

    Adition of input from keyboard

--- script.js
+++ script.js
@@ -2,107 +2,115 @@ document.getElementById("display").value = 0;
 var valor = " ";
 var acumulado = 0;
 var p_operacion = true;
-var signo = false;// es verdadero cuado a cambiado el acumulado
+// es verdadero cuado a cambiado el acumulado
+var signo = false;
 var lastOperation = null;
+var numero = null;
+var signCase = null;
 
 document.querySelectorAll("button[data-number]")
 	.forEach(button => button.addEventListener("click", e => display(e)));
 
 function display(e) {
-    const numero = e.target.dataset.number;
+    numero = e.target.dataset.number;
+    pantalla();
+     
+}
+function pantalla(){
     valor = valor+numero;
     document.getElementById("display").value = valor;  
-    signo = false;  
+    signo = false; 
 }
 document.querySelectorAll("button[data-sign]").forEach(button=>button.addEventListener("click", e => operation(e)));
 
-function operation(e){
-    switch(e.target.dataset.sign){
-        case 'add' : 
+function operation (e) {
+    signCase = e.target.dataset.sign;
+    operationCase();
+}
+function operationCase(){
+    switch (signCase) {
+        case 'add': {
             igual('suma');            
         	break;
-        case 'subtract' : 
+        }
+        case 'subtract': {
             igual('resta');
-        	break;
-        case 'multiply' :
+            break;
+        } 
+        case 'multiply': {
             igual('multi');
-        	break;
-        case 'div' : 
+            break;
+        }
+        case 'div': {
             igual('divi');
         	break;
-		case 'equal' : 
-			igual();
+        }
+        case 'equal': {
+            igual();
         	break;
-		case 'percentage' : 
-			percentage();
+        } 
+		case 'percent': {
+            percent();
         	break;
-		case 'reset' : 
-			reset();
+        }
+		case 'reset': {
+            reset();
         	break;
-		case 'erase' : 
-			erase();
+        }
+		case 'erase': {
+            erase();
         	break;
-		case 'addSub' : 
-			addSub();
+        }
+			
+		case 'addSub': {
+            addSub();
        		break;
-		case 'point' : 
-			console.log("point");
-        	break;
-    
-    }
+        }
+	}
 }
 
-function igual(operation){
+function igual(operation) {
 
-    if(valor === " "){// si no se ha puesto algun numero antes del signo + => acumulado = 0;
+    // si no se ha puesto algun numero antes del signo + => acumulado = 0;
+    if (valor === " ") {
         acumulado = 0;
     }
     else {
-        if (p_operacion === true){// valida si es la primer operacion
+
+        // valida si es la primer operacion
+        if (p_operacion === true) {
             document.getElementById("display").value = parseFloat(valor);
             acumulado = 0;
-            valor = "";
-            p_operacion = false;
-            lastOperation = operation;
-            
-           }
-        else{
+        } else {
             if (lastOperation === 'suma') {
                 document.getElementById("display").value = acumulado + parseFloat(valor);
-            }
-            else if (lastOperation === 'resta'){
+            } else if (lastOperation === 'resta') {
                 document.getElementById("display").value = acumulado - parseFloat(valor);
-            }
-            else if (lastOperation === 'multi'){
+            } else if (lastOperation === 'multi') {
                 document.getElementById("display").value = acumulado * parseFloat(valor);
-                
-            }
-            else if (lastOperation === 'divi'){
+            } else if (lastOperation === 'divi') {
                 document.getElementById("display").value = acumulado / parseFloat(valor);   
             }
-
-            valor = "";
-            lastOperation = operation;
-            
         }
+        valor = "";
+        lastOperation = operation;
         acumulado = parseFloat(document.getElementById("display").value);
         p_operacion = false;
         signo = true;
     }
     }
-function percentage(){
-    if(lastOperation === 'multi' || lastOperation === 'divi'){
+function percent() {
+    if (lastOperation === 'multi' || lastOperation === 'divi') {
         valor = valor / 100;
         document.getElementById("display").value = valor; 
-    }
-    else if(lastOperation === 'suma' ||lastOperation === 'restar'){
+    } else if (lastOperation === 'suma' ||lastOperation === 'restar') {
         valor = acumulado * valor / 100;  
         document.getElementById("display").value = valor; 
+    } else {
+        reset();
     }
-    
-    else{ reset();}
 }
-function reset(){
+function reset() {
     document.getElementById("display").value = 0;
     valor = " ";
     acumulado = 0;
@@ -111,20 +119,85 @@ function reset(){
     signo = false;
 
 }
-function addSub(){
-    if (signo){
-        acumulado = acumulado * -1; // cambia el sigo del valor acumulado despues del igual o una opereacion
+function addSub() {
+    if (signo) {
+        // cambia el sigo del valor acumulado despues del igual o una opereacion
+        acumulado = acumulado * -1; 
         document.getElementById("display").value = acumulado;
-    }
-    else{
-        valor = valor * -1;  // Cambia el signo del valor digitado
+    } else {
+        // Cambia el signo del valor digitado
+        valor = valor * -1;  
         document.getElementById("display").value = valor;
     }
 }
-function erase(){
+
+function erase() {
     valor = (valor.slice(0,((valor.length)-1)));
     if (valor == " "){
         valor = 0
     }
     document.getElementById("display").value = valor;
 }
+function manejador(elEvento) {
+    var evento = elEvento || window.event;
+    
+    if ((evento.keyCode >=48 && evento.keyCode <=56)||evento.keyCode===46 ) {
+        numero = String.fromCharCode(evento.charCode);
+        pantalla();
+    }
+    
+    switch (evento.keyCode) {
+        case 47: {
+            signCase = 'div';
+            operationCase();          
+            break;
+        }
+        case 42: {
+            signCase = 'multiply';
+            operationCase();            
+            break;
+        }
+        case 45: {
+            signCase = 'subtract';
+            operationCase();            
+            break;
+        }
+        case 43: {
+            signCase = 'add';
+            operationCase();            
+            break;
+        }
+        case 13: {
+            signCase = 'equal';
+            operationCase();  
+            break;
+        }
+        //c
+        case 99: {
+            signCase = 'reset';
+            operationCase();            
+            break;
+        }
+        case 37: {
+            signCase = 'percent';
+            operationCase();            
+            break;
+            }
+        //m menos-mas
+        case 109: {
+            signCase = 'addSub';
+            operationCase();            
+            break;
+        }
+        }
+  }
+  
+  document.onkeypress = manejador;
+  document.onkeyup = backKey;
+  function backKey(elEvento2) {
+    var evento2 = elEvento2 || window.event;
+    if (evento2.keyCode === 8) {
+        signCase = 'erase';
+        operationCase();
+    }
+  }
